ef499cde3baecdf7aff608db4293618aece72c11
Deshdeepak <rkdeshdeepak1@gmail.com>
2020-10-17 18:20:14 +0530

    Fixed 403 error

--- song.py
+++ song.py
@@ -40,42 +40,43 @@ while True:
     aurl=disp[int(choice)]["href"]
     apage=requests.get(aurl)
     asoup = BeautifulSoup(apage.content, 'html.parser')
-    songs = asoup.find_all("td")
-    songs.pop()
-    cs=0
+    songs_table = asoup.find_all("tr")
+    songs_rows = songs_table[1:]
     os.system('clear')
     print()
     print("**********************Welcome to EasyMP3***********************")
     print()
     print(asoup.find("h1").get_text())
     print()
-    ls=len(songs)/3 
-    ls=ls-1
-    for ii in songs:
-        if cs>0 and cs%3==0:
-            print(ii.get_text())
-        cs+=1
+    ls=len(songs_rows)
+    for song_row in songs_rows:
+        song = song_row.find_all("td")[0]
+        if song_row == songs_rows[-1]:
+            print("0"+str(ls) if ls < 10 else str(ls),end=" \u2015 ")
+        print(song.get_text())
     print()
     print("Enter song number to download or c to go back...")
     chosen_song=input()
     if chosen_song=="c":
         continue
     chosen_song=int(chosen_song)
-    surl=songs[3*chosen_song+1].find("a")["href"]
+    print()
+    print("1. 128 Kbps")
+    print("2. 320 Kbps")
+    chosen_format = int(input("Enter format: "))
+    surl = songs_rows[chosen_song-1].find_all("td")[chosen_format].find_next("a")["href"]
+    req = urllib.request.Request(surl,headers={'Referer': 'https://downloadming3.com/'})
+    myfile = urllib.request.urlopen(req)      
     if chosen_song == ls:
         print("Downloading zip file...")
-        myfile = requests.get(surl)
         zip_file = os.path.join(MusicFolder, str(asoup.find("h1").get_text()) +'.zip')
-        open(zip_file, 'wb').write(myfile.content)
+        open(zip_file, 'wb').write(myfile.read())
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(MusicFolder)
         os.remove(zip_file)
     else:
         print("Downloading song...")
         print(surl)
-        #myfile = requests.get(surl)
-        myfile=urllib.request.urlopen(surl)
-        print(myfile)
-        song_file = os.path.join(MusicFolder, str(songs[3*chosen_song].get_text()) +'.mp3')
-        open(song_file, 'wb').write(myfile.content)
+        song_file = os.path.join(MusicFolder, str(songs_rows[chosen_song-1].find_all("td")[0].get_text()) +'.mp3')
+        open(song_file, 'wb').write(myfile.read())
     print("Download Completed!")
